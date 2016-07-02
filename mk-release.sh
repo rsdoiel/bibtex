a710fdb17dde40a9861602771406b035bd8ed3b0
R. S. Doiel <rsdoiel@gmail.com>
2016-07-01 20:35:29 -0700

    fixed release target

--- mk-release.sh
+++ mk-release.sh
@@ -2,7 +2,7 @@
 #
 # Make releases for Linux/amd64, Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
-PROGNAME=bibtex
+PROGNAME=bibfilter
 
 env GOOS=linux GOARCH=amd64 go build -o dist/linux_amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
