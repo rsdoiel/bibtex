d8c64c5afd66d3dc381ffd16328eaca48cb3f48a
R. S. Doiel <rsdoiel@gmail.com>
2016-07-01 21:44:34 -0700

    Updated directoy names for releases

--- mk-release.sh
+++ mk-release.sh
@@ -4,7 +4,7 @@
 #
 PROGNAME=bibfilter
 
-env GOOS=linux GOARCH=amd64 go build -o dist/linux_amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 env GOOS=windows GOARCH=amd64 go build -o dist/windows/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
