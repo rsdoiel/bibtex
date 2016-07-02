9216ea272d996fd15d6e9d03062dcff608043db1
R. S. Doiel <rsdoiel@gmail.com>
2016-07-01 20:34:41 -0700

    adding legacy ARM 6 support

--- mk-release.sh
+++ mk-release.sh
@@ -5,6 +5,7 @@
 PROGNAME=bibtex
 
 env GOOS=linux GOARCH=amd64 go build -o dist/linux_amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-orig/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 env GOOS=windows GOARCH=amd64 go build -o dist/windows/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
 env GOOS=darwin	GOARCH=amd64 go build -o dist/maxosx/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
